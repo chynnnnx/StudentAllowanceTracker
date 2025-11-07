e818af82d122f5f264b1559a13e04003db04ccd0
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-07 14:16:56 +0800

    added goals entity

---
+++ StudentAllowanceTracker.Domain/Entities/Goals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAllowanceTracker.Domain.Entities
+{
+    public class Goals
+    {
+        public Guid GoalID { get; set; }
+        public string UserID { get; set; } = string.Empty;
+
+        public string GoalName { get; set; } = string.Empty;
+        public decimal TargetAmount { get; set; }
+        public decimal CurrentAmount { get; set; }
+        public DateTime TargetDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public AppIdentityUser User { get; set; } = default!;
+
+
+    }
+}
