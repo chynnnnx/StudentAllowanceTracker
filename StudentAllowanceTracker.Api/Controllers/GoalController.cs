fef2229b5e14bfdd2bab09fdcc0869745c25cb75
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-07 14:40:38 +0800

    added goalcontroller

---
+++ StudentAllowanceTracker.Api/Controllers/GoalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using StudentAllowanceTracker.Application.Commands.Goals;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace StudentAllowanceTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GoalController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("create-goal")]
+        public async Task<IActionResult> CreateGoal([FromBody] CreateGoalCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.Success)
+                return BadRequest(result.Errors);
+
+            return Ok(result.Data);
+        }
+    }
+}
