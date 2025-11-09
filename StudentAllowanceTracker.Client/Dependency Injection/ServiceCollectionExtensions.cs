c5b1adc4f931a6d8c5158a3dd939dd13d35fbb9f
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-09 11:53:12 +0800

    organize blazor client DI move services to extensions and remove duplicates

---
+++ StudentAllowanceTracker.Client/Dependency Injection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using StudentAllowanceTracker.Client.Security;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace StudentAllowanceTracker.Client.Dependency_Injection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddClientServices(this IServiceCollection services, string apiBaseUrl)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var serviceTypes = assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.Namespace != null && t.Namespace.Contains("Services"));
+
+            foreach (var type in serviceTypes)
+            {
+                foreach (var iface in type.GetInterfaces())
+                {
+                    services.AddScoped(iface, type);
+                }
+            }
+
+            services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri(apiBaseUrl),
+                DefaultRequestVersion = new Version(2, 0)
+            });
+
+            return services;
+        }
+    }
+}
--- StudentAllowanceTracker.Client/Program.cs
+++ StudentAllowanceTracker.Client/Program.cs
@@ -2,27 +2,22 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 using StudentAllowanceTracker.Client.Components;
+using StudentAllowanceTracker.Client.Dependency_Injection;
 using StudentAllowanceTracker.Client.Security;
 using StudentAllowanceTracker.Client.Services;
 using StudentAllowanceTracker.Client.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
+// Framework services
 builder.Services.AddMudServices();
 builder.Services.AddRazorPages();
 builder.Services.AddBlazoredLocalStorage();
-
 builder.Services.AddAuthorizationCore();
-
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IEmailServices, EmailServices>();
-builder.Services.AddScoped<IAllowanceService, AllowanceService>();
-builder.Services.AddScoped<IExpenseService, ExpenseService>();
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri("https://localhost:7174"),
-    DefaultRequestVersion = new Version(2, 0)
-});
+
+var apiBaseUrl = builder.Configuration.GetValue<string>("ApiSettings:BaseUrl");
+builder.Services.AddClientServices(apiBaseUrl);
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
@@ -33,7 +28,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
