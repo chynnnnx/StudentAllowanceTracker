4fdec562487a18d6b140d0f3870badb4476fd222
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-12 14:03:41 +0800

    added client budget service

---
+++ StudentAllowanceTracker.Client/Services/BudgetService.cs
@@ -0,0 +1,13 @@
+﻿using Blazored.LocalStorage;
+using StudentAllowanceTracker.Client.Services.Interfaces;
+
+namespace StudentAllowanceTracker.Client.Services
+{
+    public class BudgetService: BaseService, IBudgetService
+    {
+        public BudgetService(HttpClient httpClient, ILocalStorageService localStorage)
+          : base(httpClient, localStorage)
+        {
+        }
+    }
+}
