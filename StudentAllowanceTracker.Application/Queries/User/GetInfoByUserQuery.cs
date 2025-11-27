f83b3bee3ec339509bce834ad76b6ad5cd6cedc3
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-27 09:35:54 +0800

    commit info

---
+++ StudentAllowanceTracker.Application/Queries/User/GetInfoByUserQuery.cs
@@ -0,0 +1,13 @@
+﻿using StudentAllowanceTracker.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+namespace StudentAllowanceTracker.Application.Queries.User
+{
+    public class GetInfoByUserQuery: IRequest<AuthUserDTO>
+    {
+    }
+}
