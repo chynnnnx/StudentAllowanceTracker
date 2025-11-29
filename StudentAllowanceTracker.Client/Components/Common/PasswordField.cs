5ebf81bc5ad8b30a135a0d4caf203c3e466c4c63
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-29 20:43:18 +0800

    added password helper

---
+++ StudentAllowanceTracker.Client/Components/Common/PasswordField.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace StudentAllowanceTracker.Client.Components.Common
+{
+    public class PasswordField
+    {
+        public bool IsVisible { get; set; } = false;
+        public InputType InputType => IsVisible ? InputType.Text : InputType.Password;
+        public string Icon => IsVisible ? Icons.Material.Filled.Visibility : Icons.Material.Filled.VisibilityOff;
+
+        public void Toggle() => IsVisible = !IsVisible;
+    }
+}
---
+++ StudentAllowanceTracker.Client/Components/Common/PasswordValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentAllowanceTracker.Client.Components.Common
+{
+    public static class PasswordValidator
+    {
+        public static string ValidatePasswordStrength(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return "Password is required";
+
+            if (value.Length < 8)
+                return "Password must be at least 8 characters long";
+
+            if (!value.Any(char.IsDigit))
+                return "Password must contain at least one number";
+
+            if (!value.Any(ch => !char.IsLetterOrDigit(ch)))
+                return "Password must contain at least one special character";
+
+            return null;
+        }
+
+        public static string ValidatePasswordMatch(string password, string confirmPassword)
+        {
+            if (string.IsNullOrWhiteSpace(confirmPassword))
+                return "Please confirm your password";
+
+            if (confirmPassword != password)
+                return "Passwords do not match";
+
+            return null;
+        }
+    }
+}
