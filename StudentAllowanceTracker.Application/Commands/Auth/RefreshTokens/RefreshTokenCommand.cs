8f84c869037d1030c83598f53aa0272aed843fde
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-12-01 15:50:40 +0800

    added refresh token command

---
+++ StudentAllowanceTracker.Application/Commands/Auth/RefreshTokens/RefreshTokenCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using StudentAllowanceTracker.Application.DTOs;
+using StudentAllowanceTracker.Shared.Responses;
+
+namespace StudentAllowanceTracker.Application.Commands.Auth.RefreshTokens
+{
+    public class RefreshTokenCommand : IRequest<Result<AuthResult>>
+    {
+        public string RefreshToken { get; set; } = default!;
+    }
+}
---
+++ StudentAllowanceTracker.Application/Commands/Auth/RefreshTokens/RefreshTokenCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿    using MediatR;
+    using StudentAllowanceTracker.Application.DTOs;
+    using StudentAllowanceTracker.Application.Interfaces.Repositories;
+    using StudentAllowanceTracker.Application.Interfaces;
+    using StudentAllowanceTracker.Shared.Enums;
+    using StudentAllowanceTracker.Shared.Responses;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using StudentAllowanceTracker.Domain.Entities;
+ 
+    namespace StudentAllowanceTracker.Application.Commands.Auth.RefreshTokens
+    {
+        public class RefreshTokenCommandHandler
+         : IRequestHandler<RefreshTokenCommand, Result<AuthResult>>
+        {
+            private readonly ITokenService _tokenService;
+
+            public RefreshTokenCommandHandler(ITokenService tokenService)
+            {
+                _tokenService = tokenService;
+            }
+
+            public async Task<Result<AuthResult>> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var tokens = await _tokenService.RefreshTokensAsync(request.RefreshToken);
+                    return Result<AuthResult>.Ok(tokens);
+                }
+                catch (Exception ex)
+                {
+                    return Result<AuthResult>.Fail(ResultStatus.Failed, new[] { ex.Message });
+                }
+            }
+        }
+
+    }
